f43a4eeb83afbe663da5c92ae13b182fa84b3234
hatr1ck <38593812+hatr1ck@users.noreply.github.com>
2020-01-16 21:39:59 +0300

    Add files via upload

---
+++ build/precache-manifest.c3bc628954829e5625831f9110e99f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868442a9931394e4883fd0d79ba4e64a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8fae99461dc21228d98",
+    "url": "/static/css/main.706f3e2f.chunk.css"
+  },
+  {
+    "revision": "75b927c57373b01c7a75",
+    "url": "/static/js/2.ab5200b6.chunk.js"
+  },
+  {
+    "revision": "f09321e517ab4567bb350ec8faae3b4d",
+    "url": "/static/js/2.ab5200b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8fae99461dc21228d98",
+    "url": "/static/js/main.2937bb64.chunk.js"
+  },
+  {
+    "revision": "8fce50ef7d01a5134ada",
+    "url": "/static/js/runtime-main.88233cf2.js"
+  }
+]);
